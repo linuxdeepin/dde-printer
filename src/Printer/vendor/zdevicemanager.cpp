ffad4524bb9e19e4540b37f5c15eb0fb12af6587
Wei xie <xiewei@deepin.com>
2019-11-15 16:42:09 +0800

    fix: Fix printer name for dnssd devices
    Signed-off-by: Wei xie <xiewei@deepin.com>

--- src/Printer/vendor/zdevicemanager.cpp
+++ src/Printer/vendor/zdevicemanager.cpp
@@ -207,6 +207,9 @@ int ReflushDevicesByBackendTask::addDevices(const map<string, map<string, string
         info.strDeviceId = attrValueToQString(infomap[CUPS_DEV_ID]);
         info.strLocation = attrValueToQString(infomap[CUPS_DEV_LOCATION]);
         info.iType = InfoFrom_Detect;
+        if (uri.startsWith("dnssd://")) {
+            info.strName = info.strInfo.split("@").first().trimmed();
+        }
 
         if (0 != mergeDevice(info, backend))
             continue;
