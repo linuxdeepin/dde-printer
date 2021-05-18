5258672009b3725f47ecf1a78f7268e7a8ead090
liurui <liurui@uniontech.com>
2021-05-18 16:56:59 +0800

    fix: 欧拉版本号错误
    欧拉版本号没有显示
    
    Log: 欧拉版本号错误
    Bug: https://pms.uniontech.com/zentao/bug-view-80198.html
    Change-Id: I5016c0ca35b3251eef99d32f7e276f87ce42e576

--- src/Printer/ui/printerapplication.cpp
+++ src/Printer/ui/printerapplication.cpp
@@ -96,7 +96,11 @@ int PrinterApplication::create()
     qApp->setAttribute(Qt::AA_UseHighDpiPixmaps);
     qApp->setOrganizationName("deepin");
     qApp->setApplicationName("dde-printer");
-    qApp->setApplicationVersion(DApplication::buildVersion("1.0"));
+    if (DSysInfo::uosEditionType() == DSysInfo::UosEuler || DSysInfo::uosEditionType() == DSysInfo::UosEnterpriseC)
+        qApp->setApplicationVersion("0.7.13");
+    else {
+        qApp->setApplicationVersion(DApplication::buildVersion((QMAKE_VERSION)));
+    }
     qApp->setProductIcon(QIcon(":/images/dde-printer.svg"));
     qApp->setProductName(tr("Print Manager"));
     qApp->setApplicationDescription(tr("Print Manager is a printer management tool, which supports adding and removing printers, managing print jobs and so on."));
