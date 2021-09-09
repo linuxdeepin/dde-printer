bd28c4cc30cc5c6e4b9ab4e93a2d6eacd4f48880
ut002729 <liujinglin@uniontech.com>
2021-09-09 10:13:18 +0800

    chore: UT脚本修改
        UT脚本修改
    
    Log: UT脚本修改
    Change-Id: I24ce18d6c176a5977e5da8ebb8bfb2f8b0a21a74

--- tests/test-recoverage.sh
+++ tests/test-recoverage.sh
@@ -4,27 +4,24 @@ BUILD_DIR=build
 REPORT_DIR=report
 PROJECT_REALNAME=dde-printer
 
-cd ../
 rm -rf $BUILD_DIR
 mkdir $BUILD_DIR
 rm -f asan*
 
 cd $BUILD_DIR
-qmake CONFIG+=debug  ../src/cppcups
-make
-
-cd ../tests/
-rm -rf $BUILD_DIR
-mkdir $BUILD_DIR
-cd $BUILD_DIR
-
 qmake CONFIG+=debug ../
-make check
+make -j 8
 
-mv ../asan_printer.log* asan_${PROJECT_REALNAME}.log
-
+lcov -c -i -d ./ -o init.info
+../tests --gtest_output=xml:dde_test_report_${PROJECT_REALNAME}.xml
 lcov -c -d ./ -o coverage.info
-lcov -r coverage.info "*/tests/*" "*/usr/include*"  "*.h" "*build/*" "*/dbus/*" -o final.info
-rm -rf ../../tests/$REPORT_DIR
-mkdir -p ../../tests/$REPORT_DIR
-genhtml -o ../../tests/$REPORT_DIR final.info
+lcov -a init.info -a coverage.info -o total.info
+lcov -r total.info "*/tests/*" "*/usr/include*"  "*.h" "*build/*" "*/dbus/*" -o final.info
+
+mv asan_printer.log* asan_${PROJECT_REALNAME}.log
+
+cd ..
+rm -rf $REPORT_DIR
+mkdir -p $REPORT_DIR
+genhtml -o $REPORT_DIR ./build/final.info
+
