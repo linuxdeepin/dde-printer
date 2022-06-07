8da48a19ddb1ce1dc8ea8703e833f08df8abbe41
ut002729 <liujinglin@uniontech.com>
2022-06-07 13:46:04 +0800

    chore: CI检查项目报告路径修改
      CI检查项目报告路径修改
    
    Log: CI检查项目报告路径修改
    Change-Id: Ibbcbbefad4ea9b77e087f92a33aae45fcdf6232b

--- tests/test-recoverage.sh
+++ tests/test-recoverage.sh
@@ -28,3 +28,21 @@ rm -rf $REPORT_DIR
 mkdir -p $REPORT_DIR
 genhtml -o $REPORT_DIR ./build/final.info
 
+# CI检查项目目录修改
+BUILDUT_DIR=build-ut
+[ -d ../$BUILDUT_DIR ] && rm -rf ../$BUILDUT_DIR
+mkdir -p ../$BUILDUT_DIR
+cp -r $REPORT_DIR ../$BUILDUT_DIR
+cp $BUILD_DIR/asan_${PROJECT_REALNAME}.log ../$BUILDUT_DIR
+cp $BUILD_DIR/dde_test_report_${PROJECT_REALNAME}.xml ../$BUILDUT_DIR
+
+cd ../$BUILDUT_DIR
+mv ./report ./html
+cd html
+mv ./index.html ./cov_dde-printer.html
+chmod 755 cov_dde-printer.html
+
+cd ..
+mkdir report
+mv dde_test_report_${PROJECT_REALNAME}.xml ./report/report_${PROJECT_REALNAME}.xml
+
